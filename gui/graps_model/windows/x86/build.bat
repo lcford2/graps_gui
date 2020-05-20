0129d11eb28895afdc5a196bcddd1a574fbe7d9a
Lucas Ford <lcford2@ncsu.edu>
2020-05-20 08:43:33 -0400

    Created 32 bit (x86) and 64 bit (x64) versions of the model for windows

--- gui/graps_model/windows/x86/build.bat
+++ gui/graps_model/windows/x86/build.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
-cd ../source
+cd ../../source
 mkdir build
 cp *.f* build
 cd build
 ifort /dll path_vars.f90 definitions.f90 My_variables.f90 ffsqp.f qld.f init.f90 all_simul.f90 /link /out:res_model.dll
 rm *.f*
-mv * ../../windows
+mv * ../../windows/x86
 cd ..
 rm -r build
-cd ../windows
+cd ../windows/x86
